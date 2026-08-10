2930076fecd24709154e75bf97ba5766a6263fbd
Lucas Ikuhara <ikuhara@ibm.com>
2026-08-10 16:46:38 -0300

    feat: add rgb

--- src/models.py
+++ src/models.py
@@ -8,7 +8,6 @@ def get_thermal_detector() -> YOLO:
     Returns:
       The model instance
     """
-
     model_path = hf_hub_download(
         repo_id="foduucom/thermal-image-object-detection", filename="best.pt"
     )
@@ -16,7 +15,7 @@ def get_thermal_detector() -> YOLO:
     model = YOLO(model_path)
 
     # set model parameters
-    # params from: https://huggingface.co/foduucom/thermal-image-object-detection
+    # Reference: https://huggingface.co/foduucom/thermal-image-object-detection
     model.overrides["conf"] = 0.25  # NMS confidence threshold
     model.overrides["iou"] = 0.45  # NMS IoU threshold
     model.overrides["agnostic_nms"] = False  # NMS class-agnostic
@@ -25,10 +24,23 @@ def get_thermal_detector() -> YOLO:
     return model
 
 
-# Run prediction with configurations set directly in the predict call
-th = get_thermal_detector()
-results = th.predict(source="test.png", conf=0.25, iou=0.45)
-print(results)
-# Process or view results natively
-for r in results:
-    r.show()  # Opens the annotated image
+def get_rgb_detector() -> YOLO:
+    """
+    Instances an YOLO detector with rgb imaging weights and parameters.
+    Returns:
+      The model instance
+    """
+    # Reference: https://huggingface.co/Ultralytics/YOLOv8
+    model = YOLO("yolov8n.pt")
+
+    return model
+
+
+if __name__ == "__main__":
+
+    th = get_rgb_detector()
+    results = th.predict(source="images.jpeg", conf=0.25, iou=0.45)
+    print(results)
+    # Process or view results natively
+    for r in results:
+        r.show()  # Opens the annotated image
